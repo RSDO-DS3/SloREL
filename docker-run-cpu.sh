adfd1e09da07d620e721d22130e794826ba522e8
Slavko Zitnik <slavko@zitnik.si>
2022-09-13 04:10:39 +0200

    added restart always

--- docker-run-cpu.sh
+++ docker-run-cpu.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-docker run --rm -it --name bert_relation_extraction \
-   --platform linux/amd64 \
-       --mount type=bind,source="$(pwd)"/BERT_data,target=/BERT_data,ro \
-       -p:8000:8000 \
-       bert_relation_extraction
+docker run \
+    -d \
+    -it \
+    --restart always \
+    --name bert_relation_extraction \
+    --platform linux/amd64 \
+    --mount type=bind,source="$(pwd)"/BERT_data,target=/BERT_data,ro \
+    -p:5001:8000 \
+    bert_relation_extraction
